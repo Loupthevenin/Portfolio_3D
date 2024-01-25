c1ad3631f720216d3c9150966f328212593e4a9d
MrShadowsWolf <loup.thevenin.1@gmail.com>
2024-01-25 19:17:54 +0100

    Home Page

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { render } from "react-dom";
+
+const dom = document.getElementById("root");
+ReactDOM.createRoot(dom).render(<App />);
