1a9f168ad8bf7ee119b602d64eeebd13c0b8f43f
MrShadowsWolf <loup.thevenin.1@gmail.com>
2024-01-27 10:52:27 +0100

    Rotation, Bird + Plane

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import { Loader, Sky } from "@react-three/drei";
+import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
-import Island from "../models/Island_sea";
+import React, { Suspense, useState } from "react";
+import Island from "../models/Island";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+import Sky from "../models/Sky";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -9,27 +12,49 @@ import Island from "../models/Island_sea";
 </div> */
 }
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -10];
-    let rotation = [-1000, 44, -30];
+    let rotation = [0, 20.1, 0];
 
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
       screenPosition = [0, -6.5, -43];
     } else {
       screenScale = [1, 1, 1];
-      screenPosition = [0, 50, -350];
+      screenPosition = [0, -6.5, -43.4];
     }
 
     return [screenScale, screenPosition, rotation];
   };
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
-    <section className="w-full h-screen relative">
+    <section
+      className={`w-full h-screen relative ${
+        isRotating ? `cursor-grabbing` : `cursor-grab`
+      }`}
+    >
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
@@ -44,12 +69,23 @@ const Home = () => {
             intensity={1}
           />
 
-          <Sky />
+          <Bird />
+          <Sky isRotating={isRotating} />
 
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+
+          <Plane
+            isRotating={isRotating}
+            planePosition={planePosition}
+            planeScale={planeScale}
+            rotation={[0, 20, 0]}
           />
         </Suspense>
       </Canvas>
